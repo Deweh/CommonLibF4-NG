1e1d5d97cf51235ef825ac88f4ad1a72a1ec6ab8
DeweySalt <76853940+Deweh@users.noreply.github.com>
2024-08-29 23:04:29 -0400

    improve `hkRefPtr`

--- CommonLibF4/include/RE/Havok/hkRefPtr.h
+++ CommonLibF4/include/RE/Havok/hkRefPtr.h
@@ -6,8 +6,82 @@ namespace RE
 	class hkRefPtr
 	{
 	public:
-		~hkRefPtr() noexcept {}  // intentional
 
-		T* _ptr;
+		hkRefPtr() noexcept = default;
+
+		hkRefPtr(T* a_ptr)
+		{
+			_ptr = a_ptr;
+			TryAttach();
+		}
+
+		~hkRefPtr() noexcept
+		{
+			TryDetach();
+		}
+
+		void operator=(hkRefPtr<T>&) = delete;
+		void operator=(const hkRefPtr<T>& a_other)
+		{
+			TryDetach();
+			_ptr = a_other._ptr;
+			TryAttach();
+		}
+		
+		void operator=(hkRefPtr<T>&& a_other)
+		{
+			TryDetach();
+			_ptr = a_other._ptr;
+			a_other._ptr = nullptr;
+		}
+		void operator=(const hkRefPtr<T>&&) = delete;
+
+		T* get() const
+		{
+			return _ptr;
+		}
+
+		T* operator->() const
+		{
+			return _ptr;
+		}
+
+		T& operator*() const
+		{
+			return *_ptr;
+		}
+
+		operator bool() const
+		{
+			return _ptr != nullptr;
+		}
+
+		bool operator==(const hkRefPtr<T>& a_other)
+		{
+			return _ptr == a_other._ptr;
+		}
+
+		bool operator==(std::nullptr_t)
+		{
+			return _ptr == nullptr;
+		}
+
+	private:
+		inline void TryAttach()
+		{
+			if (_ptr) {
+				_ptr->addReference();
+			}
+		}
+
+		inline void TryDetach()
+		{
+			if (_ptr) {
+				_ptr->removeReference();
+				_ptr = nullptr;
+			}
+		}
+
+		T* _ptr{ nullptr };
 	};
 }
--- CommonLibF4/include/RE/Havok/hkReferencedObject.h
+++ CommonLibF4/include/RE/Havok/hkReferencedObject.h
@@ -13,6 +13,25 @@ namespace RE
 		static constexpr auto RTTI{ RTTI::hkReferencedObject };
 		static constexpr auto VTABLE{ VTABLE::hkReferencedObject };
 
+		inline std::int64_t addReference()
+		{
+			using func_t = decltype(&hkReferencedObject::addReference);
+			static REL::Relocation<func_t> func{ REL::ID(866015) };
+			return func(this);
+		}
+
+		inline std::int64_t removeReference()
+		{
+			using func_t = decltype(&hkReferencedObject::addReference);
+			static REL::Relocation<func_t> func{ REL::ID(1379897) };
+			return func(this);
+		}
+
+		inline std::uint32_t getReferenceCount()
+		{
+			return memSizeAndRefCount & 0xFFFF;
+		}
+
 		// add
 		virtual const hkClass* GetClassType() const { return nullptr; }             // 02
 		virtual void           DeleteThisReferencedObject() const { delete this; }  // 03
